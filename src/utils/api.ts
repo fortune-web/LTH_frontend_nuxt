c088c37d8cc9738157470c96164de169cdcf9553
fortune-web <daniel.wong9503@gmail.com>
2020-05-19 00:20:31 +0400

    add api url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -14,7 +14,7 @@ import qs from 'qs'
  * Signs all requests with an access token, if available.
  */
 export const client = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: process.env.VUE_APP_API || 'http://localhost:3000',
   paramsSerializer: (params) => {
     return qs.stringify(params, { arrayFormat: 'comma' }).replace(/,/g, '%2C')
   }
