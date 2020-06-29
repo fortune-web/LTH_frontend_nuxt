02339bdeda766a9158972166e1eb9c9e3d0b203e
fortune-web <daniel.wong9503@gmail.com>
2020-06-29 15:05:59 +0800

    fix styling issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,7 @@ export default {
   /*
    ** Global CSS
    */
-  css: [],
+  css: ['@/assets/styles/global.scss'],
   /*
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
