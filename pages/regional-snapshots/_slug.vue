d63996a666064de005002c49ac343e8e1a46d5ed
fortune-web <daniel.wong9503@gmail.com>
2021-01-02 11:07:49 -0700

    loading indicator

--- pages/regional-snapshots/_slug.vue
+++ pages/regional-snapshots/_slug.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="savedSearch" class="region">
+  <div v-loading="!savedSearch" class="region">
     <regions-hero class="region__hero" :data="savedSearch" />
     <div class="region__content">
       <search-page :saved-search="savedSearch" class="region__search" />
