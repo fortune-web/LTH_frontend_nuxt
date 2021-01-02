81df5f40ad769ac04b0fd736cea80579591b52af
fortune-web <daniel.wong9503@gmail.com>
2021-01-02 11:01:12 -0700

    add v-loading

--- pages/regional-snapshots/_slug.vue
+++ pages/regional-snapshots/_slug.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="region">
+  <div v-loading="$fetchState.pending" class="region">
     <regions-hero class="region__hero" :data="savedSearch" />
     <div class="region__content">
       <search-page :saved-search="savedSearch" class="region__search" />
